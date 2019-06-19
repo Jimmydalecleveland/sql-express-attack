5ba3aba9be7a5af77bbfdbc48f5842e897342e31
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-06-19 17:41:02 -0600

    Add race fetch, build buttons, add click to buttons

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-const state = {}
+const state = {};
 // TODO: look up why we can't store this fetch in a variable
 fetch('http://localhost:3000/players')
-  .then((res) => res.json())
-  .then((playerJson) => {
-    console.log(playerJson)
-    playerName.textContent = playerJson[0].name
-    playerStr.textContent = playerJson[0].str
-    state.playerData = playerJson[0] 
-  })
+  .then(res => res.json())
+  .then(playerJson => {
+    console.log(playerJson);
+    playerName.textContent = playerJson[0].name;
+    playerStr.textContent = playerJson[0].str;
+    state.playerData = playerJson[0];
+  });
+
+fetch('http://localhost:3000/races')
+  .then(res => res.json())
+  .then(raceJson => {
+    console.table(raceJson);
+    state.chosenRace = raceJson[0].id;
+    state.races = raceJson.reduce((stateRaces, currentRace) => {
+      const { id, ...rest } = currentRace;
+      stateRaces[id] = rest;
+      return stateRaces;
+    }, {});
+
+    Object.keys(state.races).forEach(id => {
+      const raceButton = document.createElement('button');
+      raceButton.textContent = state.races[id].name;
+      raceButton.dataset.raceId = id;
+      raceButton.addEventListener('click', handleRaceClick);
+      raceSelections.appendChild(raceButton);
+    });
+  });
 
 const playerName = document.querySelector('#playerName');
 const attackRollBtn = document.querySelector('#attackRollBtn');
 const playerStr = document.querySelector('#playerStr');
 const rollResult = document.querySelector('#rollResult');
+const raceSelections = document.querySelector('#raceSelections');
+
+function handleRaceClick(event) {
+  state.chosenRace = event.target.dataset.raceId;
+}
 
 // we need our dice to roll a number between 1-20;
 function diceRoll() {
@@ -21,9 +46,12 @@ function diceRoll() {
 
 // Displaying the dice roll to the DOM
 function attackRoll() {
-  if (state.playerData === 'undefined') return
-  const strBonus = Math.floor((state.playerData.str - 10) / 2)
-  rollResult.textContent = diceRoll() + strBonus; 
+  if (state.playerData === 'undefined') return;
+  const strBonus = Math.floor(
+    (state.playerData.str + state.races[state.chosenRace].strBonus - 10) / 2
+  );
+  // rollResult.textContent = diceRoll() + strBonus;
+  rollResult.textContent = strBonus;
 }
 
-attackRollBtn.addEventListener('click', attackRoll)
+attackRollBtn.addEventListener('click', attackRoll);
