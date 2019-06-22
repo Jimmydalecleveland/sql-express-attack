21c044d92317c8ca53abd5a8232dc199818ece65
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-06-22 12:57:57 -0600

    Added button disable functionality
    Co-authored-by: marcos-gonzales <markymarrk@gmail.com>
    Co-authored-by: elementalearth <vernonjacoba@yahoo.com>

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const state = {}
+const state = {};
+
 // TODO: look up why we can't store this fetch in a variable
 fetch('http://localhost:3000/players')
-  .then((res) => res.json())
-  .then((playerJson) => {
-    console.log(playerJson)
-    playerName.textContent = playerJson[0].name
-    playerStr.textContent = playerJson[0].str
-    state.playerData = playerJson[0] 
-  })
+  .then(res => res.json())
+  .then(playerJson => {
+    playerName.textContent = playerJson[0].name;
+    playerStr.textContent = playerJson[0].str;
+    state.playerData = playerJson[0];
+    attackRollBtn.disabled = false;
+  });
 
 const playerName = document.querySelector('#playerName');
 const attackRollBtn = document.querySelector('#attackRollBtn');
 const playerStr = document.querySelector('#playerStr');
 const rollResult = document.querySelector('#rollResult');
+
+attackRollBtn.disabled = true;
 
 // we need our dice to roll a number between 1-20;
 function diceRoll() {
@@ -21,9 +24,8 @@ function diceRoll() {
 
 // Displaying the dice roll to the DOM
 function attackRoll() {
-  if (state.playerData === 'undefined') return
-  const strBonus = Math.floor((state.playerData.str - 10) / 2)
-  rollResult.textContent = diceRoll() + strBonus; 
+  const strBonus = Math.floor((state.playerData.str - 10) / 2);
+  rollResult.textContent = diceRoll() + strBonus;
 }
 
-attackRollBtn.addEventListener('click', attackRoll)
+attackRollBtn.addEventListener('click', attackRoll);
