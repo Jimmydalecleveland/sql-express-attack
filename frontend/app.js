c96580dd92ace699fff42a0ae386b13983cf3541
Marcos Gonzales <marcosgonzales@Marcoss-MacBook-Pro.local>
2019-08-31 18:26:14 -0600

    added damageroll die damage

--- frontend/app.js
+++ frontend/app.js
@@ -4,6 +4,50 @@ const state = {
   }
 };
 
+fetch('http://localhost:80/weapons')
+.then(res => res.json())
+.then(weapon => {
+  const simpleMeleeWeaponsGroup = document.createElement('optgroup');
+  simpleMeleeWeaponsGroup.label = 'Simple melee weapons';
+ 
+  const simpleRangedWeaponsGroup = document.createElement('optgroup');
+  simpleRangedWeaponsGroup.label = 'Simple ranged weapons';
+
+  const martialMeleeWeaponsGroup = document.createElement('optgroup');
+  martialMeleeWeaponsGroup.label = 'Martial melee weapons'
+
+  const martialRangedWeaponsGroup = document.createElement('optgroup');
+  martialRangedWeaponsGroup.label = 'Martial ranged weapons'
+
+  weapon.forEach(element => {
+    const simpleMeleeWeapons = document.createElement('option')
+    
+    if(element.weaponGroup == 'simple melee weapons') {
+      simpleMeleeWeapons.innerHTML = `<option class="option-group">${element.name}</option>`
+      weaponSelect.appendChild(simpleMeleeWeaponsGroup).appendChild(simpleMeleeWeapons)
+    
+    } else if (element.weaponGroup === 'simple ranged weapons') {
+    
+      const simpleRangedWeapons = document.createElement('option')
+      simpleRangedWeapons.innerHTML = `<option class="option-group">${element.name}</option>`
+      weaponSelect.appendChild(simpleRangedWeaponsGroup).appendChild(simpleRangedWeapons)
+    
+    } else if (element.weaponGroup === 'martial melee weapons') {
+    
+      const martialMeleeWeapons = document.createElement('option');
+      martialMeleeWeapons.innerHTML = `<option class="option-group">${element.name}</option>` 
+      weaponSelect.appendChild(martialMeleeWeaponsGroup).appendChild(martialMeleeWeapons)
+    } else if (element.weaponGroup === 'martial ranged weapons'){
+      
+      const martialRangedWeapons = document.createElement('option');
+      martialRangedWeapons.innerHTML = `<option class="option-group">${element.name}</option>`
+      weaponSelect.appendChild(martialRangedWeaponsGroup).appendChild(martialRangedWeapons)
+    }
+    state.weaponDamage = weapon
+  })
+})  
+
+
 // TODO: look up why we can't store this fetch in a variable
 fetch('http://localhost:80/races')
   .then(res => res.json())
@@ -32,8 +76,11 @@ fetch('http://localhost:80/races')
 const playerName = document.querySelector('#playerName');
 const attackRollBtn = document.querySelector('#attackRollBtn');
 const playerStr = document.querySelector('#playerStr');
-const rollResult = document.querySelector('#rollResult');
+const d20Roll = document.querySelector('#rollResult');
+const damageResult = document.querySelector('#damageRoll')
 const raceSelections = document.querySelector('#raceSelections');
+const weaponSelect = document.querySelector('#weapon-select')
+let damageDieResult;
 
 function slugify(str) {
   return str.toLowerCase().replace(' ', '-');
@@ -51,7 +98,11 @@ attackRollBtn.disabled = true;
 
 // we need our dice to roll a number between 1-20;
 function diceRoll() {
-  return Math.floor(Math.random() * 20) + 1;
+  const proficency = 2
+  const strBonus = Math.floor(
+    (state.playerData.str + state.races[state.chosenRace].strBonus - 10) / 2
+  );
+  return Math.floor(Math.random() * 20 + strBonus + proficency) + 1;
 }
 
 // Displaying the dice roll to the DOM
@@ -61,7 +112,34 @@ function attackRoll() {
     (state.playerData.str + state.races[state.chosenRace].strBonus - 10) / 2
   );
   // rollResult.textContent = diceRoll() + strBonus;
-  rollResult.textContent = diceRoll() + strBonus;
+  d20Roll.textContent = 'D20 roll ' + diceRoll();
+  damageResult.textContent = `Damage roll for ${weaponName} ` + damageDie()
 }
 
-attackRollBtn.addEventListener('click', attackRoll);
+function damageDie() {
+  const strBonus = Math.floor(
+    (state.playerData.str + state.races[state.chosenRace].strBonus - 10) / 2
+  );
+  const dieSplit  = damageDieResult.split('d')
+  const damageRoll = Math.floor((Math.random() * dieSplit[1]) + strBonus) + 1
+  if(dieSplit[0] === '2') {
+    const damageRoll2 = Math.floor((Math.random() * dieSplit[1]) + strBonus) + 1
+    return damageRoll + damageRoll2
+  }
+  return damageRoll
+}
+
+attackRollBtn.addEventListener('click', attackRoll);  
+
+weaponSelect.addEventListener('change', function(e) {
+  state.weaponDamage.forEach(weapon => {
+    if(this.value === weapon.name) {
+      damageDieResult = weapon.damage
+      weaponName = weapon.name
+      damageDie()
+      return damageDieResult
+    } else {
+      return
+    }
+  })
+})
