3dd0affd506c271dadd4757d990d3461a8a7580d
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-10-06 14:14:41 -0600

    Add races post test route

--- backend/index.js
+++ backend/index.js
@@ -1,56 +1,65 @@
-const express = require('express');
-const cors = require('cors');
-const db = require('./db');
-const app = express();
-const port = process.env.NODE_ENV === 'development' ? 3000 : 80;
+const express = require('express')
+const cors = require('cors')
+const db = require('./db')
+const app = express()
+const port = process.env.NODE_ENV === 'development' ? 3000 : 80
 app.use(express.urlencoded())
 
 // DEPLOY TEST
-app.use(cors());
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Welcome to attack roll. Hit the /races endpoint for some data');
-});
+  res.send('Welcome to attack roll. Hit the /races endpoint for some data')
+})
 
 app.get('/races', (req, res) => {
   db.query('SELECT * FROM race', (error, results, fields) => {
-    if (error) throw error;
+    if (error) throw error
 
-    res.json(results);
-  });
-});
+    res.json(results)
+  })
+})
 
+app.post('/races', (req, res) => {
+  db.query(
+    'INSERT INTO race (id, name, strBonus, dexBonus) VALUES (null, "hill dwarf", 2, 0);',
+    (error, results, fields) => {
+      if (error) throw error
+
+      res.json(resuts)
+    }
+  )
+})
 
 app.get('/weapons', (req, res) => {
   db.query('SELECT * FROM weapon', (error, results, fields) => {
-    if (error) throw error;
+    if (error) throw error
 
-    res.json(results);
-  });
-});
+    res.json(results)
+  })
+})
 
 app.post('/create-race', (req, res) => {
-  const race = req.body.race;
-  const strBonus = req.body.strBonus;
-  const dexBonus = req.body.dexBonus;
-  let races = `INSERT INTO race (id, name, strBonus, dexBonus) Values(null , ?, ?, ?)`;
-  
+  const race = req.body.race
+  const strBonus = req.body.strBonus
+  const dexBonus = req.body.dexBonus
+  let races = `INSERT INTO race (id, name, strBonus, dexBonus) Values(null , ?, ?, ?)`
+
   db.query(races, [race, strBonus, dexBonus], (err, rows, fields) => {
-    if (err) throw err;
+    if (err) throw err
 
-    res.json(rows);
-  });
-});
-
+    res.json(rows)
+  })
+})
 
 app.delete('/delete-race/:id', (req, res) => {
   db.query('DELETE FROM race WHERE id = ?', req.params.id, (error, rows) => {
-    if (error) throw error;
+    if (error) throw error
 
-    res.json(rows);
-  });
-});
+    res.json(rows)
+  })
+})
 
 app.listen(port, () =>
   console.log(`RPG ATTACK ROLL SIMULATOR on port ${port}!`)
-);
+)
