62d7db85874f9b3ea85549a40e4834215891ecf4
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-10-06 15:59:01 -0600

    Add server address for fetch

--- frontend/app.js
+++ frontend/app.js
@@ -1,67 +1,67 @@
 const state = {
   playerData: {
-    str: 17
-  }
-};
+    str: 17,
+  },
+}
 
 // TODO: look up why we can't store this fetch in a variable
-fetch('http://localhost:80/races')
+fetch('http://157.245.230.125/races')
   .then(res => res.json())
   .then(raceJson => {
-    state.chosenRace = raceJson[0].id;
+    state.chosenRace = raceJson[0].id
     state.races = raceJson.reduce((stateRaces, currentRace) => {
-      const { id, ...rest } = currentRace;
-      stateRaces[id] = rest;
-      return stateRaces;
-    }, {});
+      const { id, ...rest } = currentRace
+      stateRaces[id] = rest
+      return stateRaces
+    }, {})
 
     Object.keys(state.races).forEach(id => {
-      const raceButton = document.createElement('button');
+      const raceButton = document.createElement('button')
       // raceButton.textContent = state.races[id].name;
-      const imageSlug = slugify(state.races[id].name);
-      raceButton.dataset.raceId = id;
-      raceButton.classList.add('race-button', imageSlug);
-      raceButton.innerHTML = `<img src="images/${imageSlug}.svg" />`;
-      raceButton.addEventListener('click', handleRaceClick);
-      raceSelections.appendChild(raceButton);
-    });
+      const imageSlug = slugify(state.races[id].name)
+      raceButton.dataset.raceId = id
+      raceButton.classList.add('race-button', imageSlug)
+      raceButton.innerHTML = `<img src="images/${imageSlug}.svg" />`
+      raceButton.addEventListener('click', handleRaceClick)
+      raceSelections.appendChild(raceButton)
+    })
 
-    attackRollBtn.disabled = false;
-  });
+    attackRollBtn.disabled = false
+  })
 
-const playerName = document.querySelector('#playerName');
-const attackRollBtn = document.querySelector('#attackRollBtn');
-const playerStr = document.querySelector('#playerStr');
-const rollResult = document.querySelector('#rollResult');
-const raceSelections = document.querySelector('#raceSelections');
+const playerName = document.querySelector('#playerName')
+const attackRollBtn = document.querySelector('#attackRollBtn')
+const playerStr = document.querySelector('#playerStr')
+const rollResult = document.querySelector('#rollResult')
+const raceSelections = document.querySelector('#raceSelections')
 
 function slugify(str) {
-  return str.toLowerCase().replace(' ', '-');
+  return str.toLowerCase().replace(' ', '-')
 }
 
 function handleRaceClick(event) {
   if (event.target.parentElement.classList.contains('race-button')) {
-    state.chosenRace = event.target.parentElement.dataset.raceId;
+    state.chosenRace = event.target.parentElement.dataset.raceId
   } else {
-    state.chosenRace = event.target.dataset.raceId;
+    state.chosenRace = event.target.dataset.raceId
   }
 }
 
-attackRollBtn.disabled = true;
+attackRollBtn.disabled = true
 
 // we need our dice to roll a number between 1-20;
 function diceRoll() {
-  return Math.floor(Math.random() * 20) + 1;
+  return Math.floor(Math.random() * 20) + 1
 }
 
 // Displaying the dice roll to the DOM
 function attackRoll() {
-  if (state.races === 'undefined') return;
+  if (state.races === 'undefined') return
   const strBonus = Math.floor(
     (state.playerData.str + state.races[state.chosenRace].strBonus - 10) / 2
-  );
+  )
   // rollResult.textContent = diceRoll() + strBonus;
-  rollResult.textContent = diceRoll() + strBonus;
+  rollResult.textContent = diceRoll() + strBonus
 }
 
-attackRollBtn.addEventListener('click', attackRoll);
+attackRollBtn.addEventListener('click', attackRoll)
